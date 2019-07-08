5bda657913d149ea9c5b0d1d0adb58ff5e61e587
Rinor Sadiku <rinor.sadiku2@gmail.com>
2019-07-08 23:49:08 +0200

    changed the travis file and added some configuration to the jest congif

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	setupTestFrameworkScriptFile: '<rootDir>/tests/setup.js',
 	timers: 'fake',
-	testURL: 'http://localhost:3000'
+	testURL: 'http://localhost:3000',
+	testEnvironment: 'node'
 };
